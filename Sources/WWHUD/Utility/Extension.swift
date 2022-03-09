cdda2358e8799abeb86f19e0d27a74ce9c3dc7aa
William.Weng <linuxice0609@gmail.com>
2022-03-09 11:02:04 +0800

    HUD v1.1.0

--- Sources/WWHUD/Utility/Extension.swift
+++ Sources/WWHUD/Utility/Extension.swift
@@ -288,29 +288,3 @@ extension CAAnimation {
         return self._keyframeAnimation(keyPath: animationKey, values: values, duration: duration, repeatCount: .infinity)
     }
 }
-
-// MARK: - CADisplayLink (static function)
-extension CADisplayLink {
-    
-    /// [產生CADisplayLink](https://www.hangge.com/blog/cache/detail_2278.html)
-    /// - Parameters:
-    ///   - target: AnyObject
-    ///   - selector: Selector
-    /// - Returns: CADisplayLink
-    static func _build(target: AnyObject, selector: Selector) -> CADisplayLink {
-        return CADisplayLink(target: target, selector: selector)
-    }
-}
-
-// MARK: - CADisplayLink (class function)
-extension CADisplayLink {
-    
-    /// [執行CADisplayLink Timer](https://ios.devdon.com/archives/922)
-    /// - Parameters:
-    ///   - runloop: [RunLoop](https://www.jianshu.com/p/b6ffd736729c)
-    ///   - mode: [RunLoop.Mode](https://www.hangge.com/blog/cache/detail_2278.html)
-    func _fire(to runloop: RunLoop = .main, forMode mode: RunLoop.Mode = .default) {
-        self.add(to: runloop, forMode: mode)
-        self.add(to: runloop, forMode: .tracking)
-    }
-}
